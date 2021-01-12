6b5f4ca779ef04c48512f46262080a21c627ecda
Valentin Chaveli <valen7est@gmail.com>
2021-01-12 19:17:33 +0100

    Terminada la practica AJAX

--- ajax/scripts/ajax.js
+++ ajax/scripts/ajax.js
@@ -17,7 +17,7 @@ window.addEventListener('load', (ev) => {
 });
 
 /**
- * Limpia texto y la información del los campos
+ * Limpia texto y la información de los campos
  */
 function clearFields() {
   document.querySelectorAll('span').forEach((element) => {
@@ -34,12 +34,9 @@ function procesarFetch(numsecs, user) {
   //Realiza la llamada a la API
   fetch(`${BASE_URL}${user}?delay=${numsecs}`)
     .then((response) => {
-      //Si el estado es OK se actualiza el campo estado y devuelve
-      //una promesa con los datos en json
+      //Si el estado es OK devuelve una promesa con los datos en json
       if (response.ok) {
-        status.textContent = response.status;
         return response.json();
-
         //Si el estado es incorrecto se crea un error y se salta al catch
       } else {
         status.textContent = response.status;
@@ -57,7 +54,7 @@ function procesarFetch(numsecs, user) {
       txtEmail.textContent = data.data.email;
       //------------------------------------------------
 
-      //REALIZO LA SEGUNDA LLAMADA QUE HARÁ UN POST DEL USUARIO
+      //REALIZO LA SEGUNDA LLAMADA QUE HARÁ UN POST DEL USUARIO ENCONTRADO
       fetch(POSTMAN_URL, {
         method: 'POST', //Para enviar datos
         body: JSON.stringify(data.data),
@@ -65,7 +62,13 @@ function procesarFetch(numsecs, user) {
           'Content-type': 'application/json',
         },
       })
-        .then((response) => response.json())
+        .then((response) => {
+          if (response.ok) {
+            //Muestro el estado al usuario
+            status.textContent = response.status;
+            return response.json();
+          } else throw new Error('Error al agregar el usuario');
+        })
         .then((dataPost) => {
           const idName = document.querySelector('#name');
           idName.textContent = dataPost.json.first_name;
