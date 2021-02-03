3745dcfdc662e6661b56a0635f601751a6660461
Valentin Chaveli <valen7est@gmail.com>
2021-02-03 12:10:39 +0100

    Practica JQUERY Events terminada

--- jqueryEvents/practica1/js/index.js
+++ jqueryEvents/practica1/js/index.js
@@ -1,13 +1,25 @@
 'use strict';
 $(() => {
-  let $pHTML = $('p').html();
-  let palabras = $pHTML.split(/[,.]/);
+  //Se obtiene el párrafo
+  let $pHTML = $('p');
 
-  $(palabras).each(function (indice, palabra) {
-    $(palabra).wrapInner('<b></b>');
-    console.log(palabra);
+  //Se obtiene el texto del parrafo
+  let textP = $pHTML.html().trim();
+
+  //Se obtiene cada palabra del parrafo y se almacena en un array
+  let wordsP = textP.split(' ');
+
+  //Se "encapsula"cada palabra en un contenedor span
+  let wordsWithContainer = wordsP.map((word) => `<span>${word}</span> `);
+
+  //Se agrega al parrafo las palabras encapsuladas
+  $pHTML.html(wordsWithContainer);
+
+  //Se agrega a cada span del parrafo un evento de click
+  $pHTML.on('click', 'span', function () {
+    $(this).css({
+      'background-color': 'yellow',
+      'font-weight': 'bold',
+    });
   });
-
-  console.log('-------');
-  $('body').append(palabras);
 });
